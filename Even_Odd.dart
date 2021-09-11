b27e90a0625ed2d5337d7a3848c29947af00f218
Muhammad-Bilal300 <bilalaarbi300@gmail.com>
2021-09-12 01:58:00 +0500

    Even_Odd Task

---
+++ Even_Odd.dart
@@ -0,0 +1,52 @@
+void main() {
+  num check = 3;
+  print("Number = " + (check).toString());
+  if (check % 2 == 0) {
+    print((check).toString() + " is an Even Number");
+  } else {
+    print((check).toString() + " is an Odd Number");
+  }
+
+  // ------------------------------------------------------------------------
+
+  
+  print("");
+  print("");
+  print("");
+
+
+  check = 6;
+  print("Number = " + (check).toString());
+  if (check % 2 == 0) {
+    print((check).toString() + " is an Even Number");
+  } else {
+    print((check).toString() + " is an Odd Number");
+  }
+
+
+  // ------------------------------------------------------------------------
+
+
+  print("");
+  print("");
+  print("");
+
+  check = 7;
+  print("Number = " + (check).toString());
+  if (check % 2 == 0) {
+    print((check).toString() + " is an Even Number");
+  } else {
+    print((check).toString() + " is an Odd Number");
+  }
+
+
+
+  // ------------------------------------------------------------------------
+
+
+
+  print("");
+  print("");
+  print("");
+
+}
